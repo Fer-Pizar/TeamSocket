30583316d5bdc904ffa2fd177822f6617be4fbf7
Fer-Pizar <mafer.pizarroso@gmail.com>
2026-05-10 16:25:59 -0400

    theme features y algunos detalles front

--- public/app.js
+++ public/app.js
@@ -1,4 +1,3 @@
-
 const ws = new WebSocket(`ws://${window.location.host}`);
 
 const messagesArea = document.getElementById('messages');
@@ -9,15 +8,11 @@ ws.onopen = () => console.log(' Conectado al Chat');
 
 ws.onmessage = (event) => {
     const data = JSON.parse(event.data);
-
-   
     if (data.type === 'history') {
         data.data.forEach(msg => {
             mostrarMensaje(msg.content, msg.sender_name);
         });
-    } 
-   
-    else {
+    } else {
         mostrarMensaje(data.text, data.user);
     }
 };
@@ -25,17 +20,16 @@ ws.onmessage = (event) => {
 function sendMessage() {
     const texto = messageInput.value.trim();
     let usuario = usernameInput.value.trim();
-    
-   
+
     if (usuario === "") {
         usuario = "Usuario_" + Math.floor(Math.random() * 1000);
-        usernameInput.value = usuario; 
+        usernameInput.value = usuario;
     }
 
-    if (texto !== '') { 
+    if (texto !== '') {
         const mensaje = { user: usuario, text: texto };
         ws.send(JSON.stringify(mensaje));
-        messageInput.value = ''; 
+        messageInput.value = '';
     }
 }
 
@@ -44,14 +38,38 @@ function mostrarMensaje(texto, usuario) {
     item.style.padding = "10px";
     item.style.listStyle = "none";
     item.style.borderBottom = "1px solid #eee";
-    
-    
+
     if (usuario === "Sistema") {
         item.innerHTML = `<i style="color: gray;">${texto}</i>`;
     } else {
         item.innerHTML = `<strong style="color: #007bff;">${usuario}:</strong> ${texto}`;
     }
-    
+
     messagesArea.appendChild(item);
-    messagesArea.scrollTop = messagesArea.scrollHeight; 
+    messagesArea.scrollTop = messagesArea.scrollHeight;
 }
+
+const themeSelect = document.getElementById('themeSelect');
+const themeLogo = document.getElementById('themeLogo');
+
+function applyTheme(theme) {
+    document.body.classList.remove('theme-default', 'theme-light', 'theme-dark');
+    document.body.classList.add(`theme-${theme}`);
+
+    if (theme === 'light') {
+        themeLogo.src = 'assets/light_logo.png';
+    } else {
+        themeLogo.src = 'assets/dark_logo.png';
+    }
+
+    localStorage.setItem('teamsocket-theme', theme);
+}
+
+const savedTheme = localStorage.getItem('teamsocket-theme') || 'default';
+
+applyTheme(savedTheme);
+themeSelect.value = savedTheme;
+
+themeSelect.addEventListener('change', () => {
+    applyTheme(themeSelect.value);
+});
